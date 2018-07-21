c174b5021ac95b2d2fb12e062d27b9c81d36b4d9
zhushizhang126 <zhushizhang126@qq.com>
2018-07-21 20:36:10 +0800

    bap-micro-eureka-configServer first commit

---
+++ src/main/java/com/supcon/springCloud/BapMicroEurekaConfigServerApplication.java
@@ -0,0 +1,16 @@
+package com.supcon.springCloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+@EnableDiscoveryClient
+public class BapMicroEurekaConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BapMicroEurekaConfigServerApplication.class, args);
+	}
+}
